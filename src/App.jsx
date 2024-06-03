f2d5470ff3de56c9b4a3aabf24621194261645e7
William Chan <chan.william.ts@gmail.com>
2024-06-03 12:26:38 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <h1 className="text-slate-500 underline">yo World</h1>
+    </>
+  );
+}
+
+export default App;
