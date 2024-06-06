ceef592cc1ce9208e6afaf944a55893389a3c067
William Chan <chan.william.ts@gmail.com>
2024-06-05 22:45:31 -0600

    adding a touch of interactiveness in the Intro component and adding custom scale in tailwind.config

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,14 +1,23 @@
+import me from '../assets/me.png';
+
 const Introduction = () => {
   return (
     <div>
       <div className="w-full h-60"></div>
       <div className="p-[30px] flex justify-center items-center">
         <div className="text-white max-w-3xl text-center">
-          Hi! I’m William Chan, a Frontend Developer, based in Montreal, Canada.
-          As a Junior Frontend Developer, I bring a robust set of skills
-          encompassing HTML, CSS, JavaScript, React, Tailwind, and SCSS. My
-          specialization lies in creating and maintaining responsive websites
-          that prioritize a seamless user experience.
+          Hi! I’m{' '}
+          <div className="relative inline-block group">
+            <span className="underline cursor-pointer">William Chan</span>
+            <div className="absolute z-10 scale-250 bottom-[-50px] hidden group-hover:block">
+              <img src={me} className="" alt="My picture" />
+            </div>
+          </div>
+          , a Frontend Developer, based in Montreal, Canada. As a Junior
+          Frontend Developer, I bring a robust set of skills encompassing HTML,
+          CSS, JavaScript, React, Tailwind, and SCSS. My specialization lies in
+          creating and maintaining responsive websites that prioritize a
+          seamless user experience.
         </div>
       </div>
       <div className="w-full h-60"></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      scale: {
+        250: '2.5',
+        300: '3',
+      },
       keyframes: {
         scroll: {
           '0%': { transform: 'translateY(0%)' },
